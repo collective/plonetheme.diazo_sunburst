3e5928cdeeb42abb6d94e44909cab0845742011d
Alex Clark <aclark@aclark.net>
2013-06-16 17:52:33 -0400

    Wording

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     entry_points={
-        'z3c.autoinclude.plugins': 'target = plone',
+        'z3c.autoinclude.plugin': 'target = plone',
     },
     name='diazotheme.sunburst',
     namespace_packages=[
