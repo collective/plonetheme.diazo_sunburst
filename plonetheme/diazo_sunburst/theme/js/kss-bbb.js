d64d08196f14186599a194ad66c55803a1d3202c
Alex Clark <aclark@aclark.net>
2013-06-19 19:17:35 -0400

    Bug fix

--- plonetheme/diazo_sunburst/theme/js/kss-bbb.js
+++ plonetheme/diazo_sunburst/theme/js/kss-bbb.js
@@ -58,7 +58,11 @@ function applyPortletTimeout(portlet){
 /* dom loaded related actions */
 $(document).ready(function(){
     /* Show animated spinner while AJAX is loading. */
-    var spinner = $('<div id="ajax-spinner"><img src="' + portal_url + '/spinner.gif" alt=""/></div>');
+/*    var spinner = $('<div id="ajax-spinner"><img src="' + portal_url + '/spinner.gif" alt=""/></div>'); */
+
+    /* XXX Replace spinner with… Plone logo! ;-) Not sure how to work around not having a proper portal_url. */
+    var spinner = $('<div id="ajax-spinner"><img src=/++resource++plone-logo.png/spinner.gif" alt=""/></div>');
+
     spinner.appendTo('body').hide();
     $(document).ajaxStart(function() { spinner.show(); });
     $(document).ajaxStop(function() { spinner.hide(); });
